a3b274a47a4509599e31876faf235535ff43fa67
Yurii Kopiika <ykopiika@e1r1p10.unit.ua>
2020-09-17 17:49:15 +0300

    add components && made tasks working on semaphores

--- t00/main/main.c
+++ t00/main/main.c
@@ -1,8 +1,67 @@
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "driver/gpio.h"
+#include "main.h"
+
+static TaskHandle_t oled_tsk = NULL;
+static xSemaphoreHandle xMutex;
+//static TaskHandle_t photoresistor_tsk = NULL;
+
+
+static void read_photoresistor(void *param)
+{
+    t_app *a = (t_app*)param;
+    int res = 0;
+    _Bool on = true;
+    while(on)
+    {
+        if (xSemaphoreTake(xMutex, (TickType_t)0xffffffff) == 1)
+        {
+            if (oled_tsk != NULL)
+            {
+                xTaskNotify(oled_tsk, res, eSetValueWithOverwrite);
+                res++;
+            }
+            xSemaphoreGive(xMutex);
+        }
+        else
+            vTaskDelay(10 / portTICK_PERIOD_MS);
+    }
+}
+
+static void oled_brightness(void *param)
+{
+    uint32_t res = 0;
+    _Bool on = true;
+    while(on)
+    {
+        if (xSemaphoreTake(xMutex, (TickType_t)0xffffffff) == 1)
+        {
+            if(xTaskNotifyWait(0, 0, &res, portMAX_DELAY) == pdTRUE)
+            {
+                printf("%d\n", res);
+            }
+            xSemaphoreGive(xMutex);
+        }
+        else
+            vTaskDelay(10 / portTICK_PERIOD_MS);
+    }
+}
+
+static void init_app(t_app *app)
+{
+    memset(app, 0, sizeof(*app));
+    init_oled(&app->oled);
+    memset(&app->oled.pixels, 255, sizeof(app->oled.pixels));
+    display_pixels(&app->oled);
+}
 
 void app_main(void)
 {
+    t_app app;
+    init_app(&app);
 
+    xMutex = xSemaphoreCreateMutex();
+
+    xTaskCreate(read_photoresistor, "photoresistor",
+                2048, &app, 10, NULL);
+    xTaskCreate(oled_brightness, "oled_brightness",
+                2048, &app, 10, &oled_tsk);
 }
---
+++ t00/main/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include "oled.h"
+
+typedef struct  s_app
+{
+    t_oled          oled;
+//    TaskHandle_t    oled_tsk;
+//    TaskHandle_t    photoresistor_tsk;
+}               t_app;
+
+#endif
